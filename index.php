18c7a22ce53abc482efb6a5fa27ddc797be2fd76
dbranch6 <dbranch6@cnm.edu>
2017-08-01 16:40:02 -0600

    created bootstrapfun html

--- index.php
+++ index.php
@@ -1,8 +1,46 @@
 <!DOCTYPE>
 <html lang="en">
-	<head></head>
-	<body>
+	<head><meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<div class="jumbotron">
+			<h1>Hello, world!</h1>
+			<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information. </p>
+			<p><a class="btn btn-primary btn-lg" href="#" role="button"></a></p>
+		</div></head>
+	<body>
+		<title></title>
+
+			<div class="container">
+
+				<div class="row">
+					<div class="col-md-3"> Column One: 3/6 columns, 50% of container width. </div>
+					<div class="col-md-3"> Column Two: 3/6 columns, 50% of container width. </div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-4"> Column Three: 2/6 columns, 33.33% of container width. </div>
+					<div class="col-md-4"> Column Four: 2/6 columns, 33.33% of container width. </div>
+					<div class="col-md-4"> Column Five: 2/6 columns, 33.33% of container width. </div>
+				</div>
+
+			</div>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="js/bootstrap.min.js"></script>
 	</body>
 
 </html>

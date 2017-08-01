e5bd90eba13a2a0ca0e63629d722c9cfae3b2a5d
dbranch6 <dbranch6@cnm.edu>
2017-08-01 16:42:45 -0600

    changed bootstrap

--- index.php
+++ index.php
@@ -14,23 +14,24 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+		</head>
+	<body>
+
 		<div class="jumbotron">
 			<h1>Hello, world!</h1>
 			<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information. </p>
 			<p><a class="btn btn-primary btn-lg" href="#" role="button"></a></p>
-		</div></head>
-	<body>
-		<title></title>
+		</div>
 
 			<div class="container">
 
 				<div class="row">
-					<div class="col-md-3"> Column One: 3/6 columns, 50% of container width. </div>
-					<div class="col-md-3"> Column Two: 3/6 columns, 50% of container width. </div>
+					<div class="col-md-3"> Column One: 6/12 columns, 50% of container width. </div>
+					<div class="col-md-3"> Column Two: 6/12 columns, 50% of container width. </div>
 				</div>
 
 				<div class="row">
-					<div class="col-md-4"> Column Three: 2/6 columns, 33.33% of container width. </div>
+					<div class="col-md-4"> Column Three: 3/12 columns, 33.33% of container width. </div>
 					<div class="col-md-4"> Column Four: 2/6 columns, 33.33% of container width. </div>
 					<div class="col-md-4"> Column Five: 2/6 columns, 33.33% of container width. </div>
 				</div>
